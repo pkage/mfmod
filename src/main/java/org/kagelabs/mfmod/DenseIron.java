266a00f07f326ad43e00053399b97f5dd980fc7b
quadnix <quadnix1@gmail.com>
2015-03-18 00:18:56 -0400

    added dense iron

--- src/main/java/org/kagelabs/mfmod/DenseIron.java
+++ src/main/java/org/kagelabs/mfmod/DenseIron.java
@@ -2,6 +2,7 @@ package org.kagelabs.mfmod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 
 /**
  * Created by patrick on 3/17/15.
@@ -19,5 +20,6 @@ public class DenseIron extends Block{
         name = Mfmod.MODID + "_denseiron";
         this.setBlockName(name);
         this.setBlockTextureName(Mfmod.MODID + ":denseiron");
+        this.setCreativeTab(CreativeTabs.tabBlock);
     }
 }
