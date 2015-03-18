ed4e9b95da9716becbf89c82f0755b992ddab44d
quadnix <quadnix1@gmail.com>
2015-03-17 23:01:56 -0400

    Added mfmod stuff

--- src/main/java/org/kagelabs/mfmod/DenseIron.java
+++ src/main/java/org/kagelabs/mfmod/DenseIron.java
@@ -2,13 +2,25 @@ package org.kagelabs.mfmod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 
 /**
  * Created by patrick on 3/17/15.
  */
 public class DenseIron extends Block{
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+
     DenseIron() {
         super(Material.rock);
-
+        this.name = Mfmod.MODID + "_denseIron";
+        setBlockName(this.name);
+        setBlockTextureName(Mfmod.MODID + ":denseIron");
+        setCreativeTab(CreativeTabs.tabBlock);
     }
+
+
 }
