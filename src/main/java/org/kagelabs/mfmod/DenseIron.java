329816c157c398a9ce4009cba0d0b46ee241a57a
quadnix <quadnix1@gmail.com>
2015-03-17 22:40:44 -0400

    Added mfmod stuff

---
+++ src/main/java/org/kagelabs/mfmod/DenseIron.java
@@ -0,0 +1,14 @@
+package org.kagelabs.mfmod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+/**
+ * Created by patrick on 3/17/15.
+ */
+public class DenseIron extends Block{
+    DenseIron() {
+        super(Material.rock);
+
+    }
+}
---
+++ src/main/java/org/kagelabs/mfmod/Mfmod.java
@@ -0,0 +1,19 @@
+package org.kagelabs.mfmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by patrick on 3/17/15.
+ */
+
+@Mod(modid = Mfmod.MODID, version = Mfmod.VERSION)
+public class Mfmod {
+    public static final String MODID = "mfmod";
+    public static final String VERSION = "0.0.1";
+
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent e) {
+
+    }
+}
