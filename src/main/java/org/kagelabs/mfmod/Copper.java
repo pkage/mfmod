54013b6b5b6802ac9e946806d73ca0c6262051c2
Philip Xu <philipxjm1@gmail.com>
2015-03-17 23:07:24 -0400

    wtf

---
+++ src/main/java/org/kagelabs/mfmod/Copper.java
@@ -0,0 +1,16 @@
+package org.kagelabs.mfmod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+
+/**
+ * Created by Philip on 3/17/15.
+ */
+public class Copper extends Block{
+    Copper(){
+        super(Material.rock);
+        setBlockName(Mfmod.MODID + "_" + "Copper Ore");
+        setCreativeTab(CreativeTabs.tabBlock);
+    }
+}
--- src/main/java/org/kagelabs/mfmod/Mfmod.java
+++ src/main/java/org/kagelabs/mfmod/Mfmod.java
@@ -2,7 +2,6 @@ package org.kagelabs.mfmod;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
 
 /**
  * Created by patrick on 3/17/15.
@@ -13,11 +12,8 @@ public class Mfmod {
     public static final String MODID = "mfmod";
     public static final String VERSION = "0.0.1";
 
-    private DenseIron denseIron;
-
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent e) {
-        denseIron = new DenseIron();
-        GameRegistry.registerBlock(denseIron, denseIron.getName());
+
     }
 }
