a4cf1ea7ad55e84b4a269934c357742a638419fd
quadnix <quadnix1@gmail.com>
2015-03-17 23:21:18 -0400

    added denseiron stuff

--- src/main/java/org/kagelabs/mfmod/DenseIron.java
+++ src/main/java/org/kagelabs/mfmod/DenseIron.java
@@ -7,8 +7,17 @@ import net.minecraft.block.material.Material;
  * Created by patrick on 3/17/15.
  */
 public class DenseIron extends Block{
+
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+
     DenseIron() {
         super(Material.rock);
-
+        name = Mfmod.MODID + "_denseiron";
+        this.setBlockName(name);
+        this.setBlockTextureName(Mfmod.MODID + ":denseiron");
     }
 }
