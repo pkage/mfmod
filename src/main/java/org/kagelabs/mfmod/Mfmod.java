1ab67b8a520e7c5b7d5aaacb23652be908df200e
quadnix <quadnix1@gmail.com>
2015-03-17 23:23:25 -0400

    removed git barf

--- src/main/java/org/kagelabs/mfmod/Mfmod.java
+++ src/main/java/org/kagelabs/mfmod/Mfmod.java
@@ -17,7 +17,6 @@ public class Mfmod {
     public static final String MODID = "mfmod";
     public static final String VERSION = "0.0.1";
 
-<<<<<<< HEAD
     private DenseIron denseIron;
     public Copper copper;
 
