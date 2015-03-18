4e250e19303d6656d4c773c445826bde82eee988
quadnix <quadnix1@gmail.com>
2015-03-17 23:43:53 -0400

    added denseiron stuff

--- src/main/java/org/kagelabs/mfmod/Mfmod.java
+++ src/main/java/org/kagelabs/mfmod/Mfmod.java
@@ -17,7 +17,7 @@ public class Mfmod {
     public static final String MODID = "mfmod";
     public static final String VERSION = "0.0.1";
 
-    private DenseIron denseIron;
+    public DenseIron denseIron;
     public CopperBlock copperblock;
 
     @Mod.EventHandler
